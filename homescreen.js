50bb850c0ae9c7a8c44c86a484f4cce42153e7ef
madelennie <nordlund.jennie@gmail.com>
2018-10-29 11:57:33 +0100

    Initial commit

---
+++ homescreen.js
@@ -0,0 +1,29 @@
+
+// function initElement()
+// {
+//     var showMouse = document.getElementByClassName("div");
+//
+//     div.onmouseover = showMouseOver;
+//     div.onmouseout = showMouseOut;
+// };
+//
+// function showMouseOver()
+// {
+//     var notice = document.getElementById("notice");
+//     notice.innerHTML.background = 'rgb(94, 90, 88, 0.5)';
+// }
+//
+// function showMouseOut()
+// {
+//     var notice = document.getElementById("notice");
+//     notice.innerHTML.background = 'rgb(94, 90, 88)';
+// }
+
+
+  function mOver(obj) {
+      mouseOver.style.backgroundColor = "rgba(94, 90, 88, 0.8)";
+  }
+
+  function mOut(obj) {
+      mouseOver.style.backgroundColor = "rgb(94, 90, 88)";
+  }
